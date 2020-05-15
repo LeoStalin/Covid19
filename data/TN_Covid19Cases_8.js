d9e47b4e881dec3d1a17b004415c9bd5ab0d940d
LeoStalin <39996089+LeoStalin@users.noreply.github.com>
2020-05-15 15:03:39 +0530

    Add files via upload

---
+++ data/TN_Covid19Cases_8.js
@@ -0,0 +1,8 @@
+var json_TN_Covid19Cases_8 = {
+"type": "FeatureCollection",
+"name": "TN_Covid19Cases_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "LAYER": "Unknown Point Feature", "POINT_SYMB": "Dot" }, "geometry": { "type": "Point", "coordinates": [ 79.19164609145146, 9.285824066593197 ] } }
+]
+}
